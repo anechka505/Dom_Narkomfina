6d373f596cb8d8903acf1373f854bcd4eca6ed19
anechka505 <tapkatron@gmail.com>
2025-10-11 09:47:14 +0300

    дз 11.10.25
    Добавила содержание и сайдбар, плавный скролл, смену цвета при наведении

---
+++ javascripts/sidebar.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('.sidebar a').each(function () {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            var scrollTo = $(this).attr('href');
+            var whitespace = scrollTo == '#s1' ? 70 : 30;
+            $('html, body').animate({
+                scrollTop: $(scrollTo).offset().top - whitespace
+            }, 1000);
+        });
+    });
+});
+
+$(function () {
+    $('.content a').each(function () {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            var scrollTo = $(this).attr('href');
+            var whitespace = scrollTo == '#s1' ? 70 : 30;
+            $('html, body').animate({
+                scrollTop: $(scrollTo).offset().top - whitespace
+            }, 1000);
+        });
+    });
+});
